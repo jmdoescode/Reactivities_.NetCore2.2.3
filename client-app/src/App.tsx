323587d5b72d4c2a6a91f2db1287bc136a841043
JM De Ocampo <jmdeocampo@outlook.com>
2019-10-28 13:48:20 -0700

    3.029 - Use React State - add state object, add componentDidMount, and loop over values in the render method

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -5,9 +5,28 @@ import { cars } from './demo';
 import CarItem from './CarItem';
 
 class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: [{id:1, name: 'Value 101'}, {id:2, name: 'Value 102'}]
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
+        </header>
+
         <ul>
           {cars.map((car) => (
             // <li>{car.color}</li> //#1 create a CarItem component instead
