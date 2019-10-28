e1addd52df12803d14cdf1b68343a5bdc8771642
JM De Ocampo <jmdeocampo@outlook.com>
2019-10-28 13:28:59 -0700

    3.028 - React Class Components - Changed the App to be a class componenet instead of a react functional component.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { cars } from './demo';
 import CarItem from './CarItem';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <ul>
-        {cars.map((car) => (
-          // <li>{car.color}</li> //#1 create a CarItem component instead
-          <CarItem car={car} />
-        ))}
-      </ul>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {cars.map((car) => (
+            // <li>{car.color}</li> //#1 create a CarItem component instead
+            <CarItem car={car} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
