ff42a35499c5706ab1b5f6c0c0f92ed59d3f7cb6
JM De Ocampo <jmdoescode@gmail.com>
2019-11-01 13:22:58 -0700

    4.047 - Remove unnecessary using statements

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,6 @@
-﻿using System.Runtime.CompilerServices;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Persistence;
