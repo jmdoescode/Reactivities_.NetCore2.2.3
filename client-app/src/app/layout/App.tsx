a74fa30ee9bd546df6c94a63be30dd1036ef6e95
JM De Ocampo <jmdoescode@gmail.com>
2019-11-04 09:22:36 -0800

    5.056 - Styling React Components - Using Fragments

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react'
+import React, { useState, useEffect, Fragment } from 'react';
+import { Header, Icon, List, Container } from 'semantic-ui-react'
 import axios from 'axios'
 import NavBar from './../../features/nav/NavBar';
 import { IActivity } from './../models/activity';
@@ -15,14 +15,16 @@ const App = () => {
   }, []); //empty array ensures that useEffect will run 1 time only - every time this component renders the useEffect method will be called
 
   return (
-    <div>
+    <Fragment>
       <NavBar />
-      <List>
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+      <Container style={{marginTop: '7em'}}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 }
 
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <Menu fixed="top" inverted>
             <Container>
                 <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo" />
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: 10}} /> {/* or you can do {marginRight: '10px'} as a string */}
                     Reactivities
                 </Menu.Item>
                 <Menu.Item name='Activities'/>
