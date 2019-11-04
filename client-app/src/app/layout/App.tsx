f2aca0ef05548f0077e56130a951d0e24550887f
JM De Ocampo <jmdoescode@gmail.com>
2019-11-03 22:48:50 -0800

    5.054 - Refactor class components using React Hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
 import axios from 'axios'
 import { IActivity } from './../../models/activity';
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([])
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities')
-      .then((response) => {
-        this.setState({
-          activities: response.data
-        })
-      })
-  }
+      .then(response => {
+        setActivities(response.data)
+      });
+  }, []); //empty array ensures that useEffect will run 1 time only - every time this component renders the useEffect method will be called
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
