aaf44a3cd0883613d583e0cd64576ba5bb07fa6b
JM De Ocampo <jmdoescode@gmail.com>
2019-11-03 18:33:20 -0800

    5.051 - Folder structure in React - updating folder structure & moving/deleting files

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
-import './App.css';
-import { cars } from './demo';
-import CarItem from './CarItem';
 import axios from 'axios'
 
 class App extends Component {
