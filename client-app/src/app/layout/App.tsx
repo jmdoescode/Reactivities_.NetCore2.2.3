6bffcc7699422455f5861bb9878ad6f20cb8b09b
JM De Ocampo <jmdoescode@gmail.com>
2019-11-04 13:59:09 -0800

    5.061 - Selecting an individual activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,39 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import { Header, Icon, List, Container } from 'semantic-ui-react'
-import axios from 'axios'
-import NavBar from './../../features/nav/NavBar';
-import { IActivity } from './../models/activity';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import React, { useState, useEffect, Fragment } from "react";
+import { Header, Icon, List, Container } from "semantic-ui-react";
+import axios from "axios";
+import NavBar from "./../../features/nav/NavBar";
+import { IActivity } from "./../models/activity";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 const App = () => {
-  const [activities, setActivities] = useState<IActivity[]>([])
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>();
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0]);
+  };
 
   useEffect(() => {
-    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
       .then(response => {
-        setActivities(response.data)
+        setActivities(response.data);
       });
   }, []); //empty array ensures that useEffect will run 1 time only - every time this component renders the useEffect method will be called
 
   return (
     <Fragment>
       <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities={activities} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          //selectedActivity={selectedActivity!} //use exclamation mark defines it as an activity or null (overrides the type safety)
+          selectedActivity={selectedActivity!} //better to define union type as null in ActivityDashboard
+        />
       </Container>
     </Fragment>
   );
-}
+};
 
 export default App;
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import { Grid, List } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
-import ActivityList from './ActivityList'
-import ActivityDetails from './../details/ActivityDetails';
-import ActivityForm from './../form/ActivityForm';
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "./../details/ActivityDetails";
+import ActivityForm from "./../form/ActivityForm";
 
 interface IProps {
-    activities: IActivity[];
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({activities}) => {
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <ActivityList activities={activities}></ActivityList>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <ActivityDetails />
-                <ActivityForm />
-            </Grid.Column>
-        </Grid>
-    )
-}
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity
+}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} selectActivity={selectActivity}
+        ></ActivityList>
+      </Grid.Column>
+      <Grid.Column width={6}>
+        { selectedActivity && <ActivityDetails activity={selectedActivity} /> } {/* && - everything to the right only display if it is not null*/}
+        <ActivityForm />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
-export default ActivityDashboard
+export default ActivityDashboard;
