4b8fdd2c949e8d80c9083ccda807405536ef950b
JM De Ocampo <jmdoescode@gmail.com>
2019-11-06 12:09:40 -0800

    7.089 - Adding delete activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,28 +7,14 @@ import ActivityForm from "./../form/ActivityForm";
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore'
 
-interface IProps {
-  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-  submitting: boolean;
-  target: string;
-}
-
-const ActivityDashboard: React.FC<IProps> = ({
-  deleteActivity,
-  submitting,
-  target
-}) => {
+const ActivityDashboard: React.FC = () => {
   const activityStore = useContext(ActivityStore);
   const {activities, editMode, selectedActivity, selectActivity} = activityStore;
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList
-          deleteActivity={deleteActivity}
-          submitting={submitting}
-          target={target}
-        ></ActivityList>
+        <ActivityList />
       </Grid.Column>
       <Grid.Column width={6}>
         {/* && - everything to the right only display if it is not null*/}
