0f5b5711e2e074f2873b868f91ec16a95e0653d1
JM De Ocampo <jmdoescode@gmail.com>
2019-11-06 17:27:41 -0800

    8.097 - Adding NavLink - Make home an exact path

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar: React.FC = () => {
   return (
     <Menu fixed="top" inverted>
       <Container>
-        <Menu.Item header as={NavLink} to='/'>
+        <Menu.Item header as={NavLink} exact to='/'>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />{" "}
           {/* or you can do {marginRight: '10px'} as a string */}
           Reactivities
