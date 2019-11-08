d28580d93b13114d895b26f570326bab4dacd0b2
JM De Ocampo <jmdoescode@gmail.com>
2019-11-08 00:35:53 -0800

    Revert "9.111 - Group Activities by Date in the ActivityList"
    This reverts commit 07ad85ed0f2b5edf9f8694876f780e7418ac4bcd.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,18 +13,9 @@ class ActivityStore {
     @observable target = '';
 
     @computed get activitiesByDate() {
-        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
-    }
-
-    groupActivitiesByDate(activities: IActivity[]){
-        const sortedActivities = activities.sort(
+        return Array.from(this.activityRegistry.values()).sort(
             (a, b) => Date.parse(a.date) - Date.parse(b.date)
-        )
-        return Object.entries(sortedActivities.reduce((activities, activity) => {
-            const date = activity.date.split('T')[0];
-            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
-            return activities;
-        }, {} as {[key:string]: IActivity[]}));
+        );
     }
 
     @action loadActivities = async () => {
@@ -39,7 +30,6 @@ class ActivityStore {
                 });
                 this.loadingInitial = false
             });
-            console.log(this.groupActivitiesByDate(activities));
         } catch (error){
             runInAction('load activities error', () => {
                 this.loadingInitial = false //7.084 - in the catch as well so that if we have a problem it'll stay in loading
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,30 +1,21 @@
-import React, { useContext, Fragment } from "react";
-import { Item, Segment, Label } from "semantic-ui-react";
-import { observer } from "mobx-react-lite";
-import ActivityStore from "../../../app/stores/activityStore";
-import ActivityListItem from "./ActivityListItem";
+import React, { useContext } from "react";
+import { Item, Segment } from "semantic-ui-react";
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore'
+import ActivityListItem from './ActivityListItem';
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const { activitiesByDate } = activityStore;
+  const {activitiesByDate} = activityStore;
 
   return (
-    <Fragment>
-      {activitiesByDate.map(([group, activities]) => (
-        <Fragment>
-          <Label size="large" color="blue">
-            {group}
-          </Label>
-          <Segment clearing>
-            <Item.Group divided>
-              {activities.map(activity => (
-                <ActivityListItem key={activity.id} activity={activity} />
-              ))}
-            </Item.Group>
-          </Segment>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Segment clearing>
+      <Item.Group divided>
+        {activitiesByDate.map(activity => (
+          <ActivityListItem key={activity.id} activity={activity} />
+        ))}
+      </Item.Group>
+    </Segment>
   );
 };
 
