022a348590677660b44c9c366b79c7346a8a4112
JM De Ocampo <jmdoescode@gmail.com>
2019-11-08 00:36:18 -0800

    Revert "Revert "9.111 - Group Activities by Date in the ActivityList""
    This reverts commit d28580d93b13114d895b26f570326bab4dacd0b2.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,30 @@
-import React, { useContext } from "react";
-import { Item, Segment } from "semantic-ui-react";
-import { observer } from 'mobx-react-lite';
-import ActivityStore from '../../../app/stores/activityStore'
-import ActivityListItem from './ActivityListItem';
+import React, { useContext, Fragment } from "react";
+import { Item, Segment, Label } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import ActivityStore from "../../../app/stores/activityStore";
+import ActivityListItem from "./ActivityListItem";
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const {activitiesByDate} = activityStore;
+  const { activitiesByDate } = activityStore;
 
   return (
-    <Segment clearing>
-      <Item.Group divided>
-        {activitiesByDate.map(activity => (
-          <ActivityListItem key={activity.id} activity={activity} />
-        ))}
-      </Item.Group>
-    </Segment>
+    <Fragment>
+      {activitiesByDate.map(([group, activities]) => (
+        <Fragment>
+          <Label size="large" color="blue">
+            {group}
+          </Label>
+          <Segment clearing>
+            <Item.Group divided>
+              {activities.map(activity => (
+                <ActivityListItem key={activity.id} activity={activity} />
+              ))}
+            </Item.Group>
+          </Segment>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
