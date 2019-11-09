f6114341db5a528f466c93741b68ec449136e271
JM De Ocampo <jmdoescode@gmail.com>
2019-11-08 20:41:08 -0800

    10.133 - Adding toast notifications

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from './../models/activity';
 import { history } from '../..'; //10.131 - if a file is called index.tsx then it doesn't need to be specified
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'http://localhost:5000/api'; //6.071 - every api request will use this root path
 
@@ -11,6 +12,9 @@ axios.interceptors.response.use(undefined, error => {
     }
     if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')){
         history.push('/notfound');
+    }
+    if (status === 500) {
+        toast.error('Server error - check the terminal for more info!');
     }
 });
 
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,10 +8,12 @@ import { Route, withRouter, RouteComponentProps, Switch } from "react-router-dom
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import NotFound from './NotFound';
+import { ToastContainer } from 'react-toastify';
 
 const App: React.FC<RouteComponentProps> = ({location}) => {
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' />
       <Route exact path='/' component={HomePage} />
       <Route path={'/(.+)'} render={() => (
         <Fragment>
