eb9ad0fe87f043d05aaa72e498cc5d65d3d28acd
JM De Ocampo <jmdoescode@gmail.com>
2019-11-10 20:20:02 -0800

    11.139 - Refactoring the ActivityForm to use react-final-form

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,6 +6,7 @@ import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router";
 import { Form as FinalForm, Field } from "react-final-form";
+import { TextInput } from './../../../app/common/form/TextInput';
 
 interface DetailParams {
   id: string;
@@ -90,41 +91,39 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
             onSubmit={handleFinalFormSubmit}
             render={({ handleSubmit }) => (
               <Form onSubmit={handleSubmit}>
-                <Form.Input
+                <Field
                   onChange={handleInputChange}
                   name="title"
                   placeholder="Title"
                   value={activity.title}
                   component='input'
                 />
-                <Form.TextArea
-                  onChange={handleInputChange}
+                <Field
+                  component={TextInput}
                   name="description"
-                  rows={2}
                   placeholder="Description"
                   value={activity.description}
                 />
-                <Form.Input
-                  onChange={handleInputChange}
+                <Field
+                  component={TextInput}
                   name="category"
                   placeholder="Category"
                   value={activity.category}
                 />
-                <Form.Input
-                  onChange={handleInputChange}
+                <Field
+                  component={TextInput}
                   name="date"
-                  type="datetime-local"
                   placeholder="Date"
                   value={activity.date}
                 />
-                <Form.Input
-                  onChange={handleInputChange}
+                <Field
+                  component={TextInput}
                   name="city"
                   placeholder="City"
                   value={activity.city}
                 />
-                <Form.Input
-                  onChange={handleInputChange}
+                <Field
+                  component={TextInput}
                   name="venue"
                   placeholder="Venue"
                   value={activity.venue}
