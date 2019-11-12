9aedb8443f5ab26f03825388fc8cf76985b55fdc
JM De Ocampo <jmdoescode@gmail.com>
2019-11-11 18:09:44 -0800

    12.160 - JSON Web token intro - also added user and will use User for LoginHandler as well as AppUser

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -11,7 +11,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -26,7 +26,7 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
@@ -35,7 +35,7 @@ namespace Application.User
                 _signInManager = signInManager;
                 _userManager = userManager;
             }
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
@@ -46,7 +46,13 @@ namespace Application.User
                 if(result.Succeeded)
                 {
                     //TODO: generate token
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "This will be a token",
+                        Username = user.UserName,
+                        Image = null
+                    };
                 }
 
                 throw new RestException(HttpStatusCode.Unauthorized);
