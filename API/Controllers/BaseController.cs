5dfb0649ee8ac0e58acddfc7a65541fc0a5b9ca2
JM De Ocampo <jmdoescode@gmail.com>
2019-11-11 16:39:55 -0800

    12.158 - Adding a Base API Controller

---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
