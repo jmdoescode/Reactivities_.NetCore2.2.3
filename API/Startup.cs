b064f729693737c954dea72c9fffea7dcac32f21
JM De Ocampo <jmdoescode@gmail.com>
2019-11-11 20:07:49 -0800

    12.164 - Generating a JWT Token

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System.Reflection.PortableExecutable;
 using API.Middleware;
+using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -47,6 +49,8 @@ namespace API
             var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
