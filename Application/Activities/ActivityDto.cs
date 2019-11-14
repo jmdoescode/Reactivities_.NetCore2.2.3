ce4ca51733383eb9e84a0b8f6936cbdf28ade816
JM De Ocampo <jmdoescode@gmail.com>
2019-11-13 21:24:30 -0800

    14.199 - Configuring AutoMapper mappings

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Application.Activities
 {
@@ -12,7 +13,7 @@ namespace Application.Activities
         public DateTime Date { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-
-        public ICollection<AttendeeDto> Attendees {get; set;}
+        [JsonProperty("attendees")]
+        public ICollection<AttendeeDto> UserActivities {get; set;}
     }
 }
