ae5bf9b3d0548339e37313bd9f0fcc6f0d418734
JM De Ocampo <jmdoescode@gmail.com>
2019-11-17 20:23:40 -0800

    17.xxx - Decrease the loading time manually

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -38,10 +38,10 @@ const sleep = (ms: number) => (response: AxiosResponse) =>
     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
 
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody) 
+    get: (url: string) => axios.get(url).then(sleep(500)).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(sleep(500)).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(500)).then(responseBody),
+    del: (url: string) => axios.delete(url).then(sleep(500)).then(responseBody) 
 };
 
 const Activities = {
