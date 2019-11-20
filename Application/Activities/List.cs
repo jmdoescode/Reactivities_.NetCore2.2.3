c1cd84d4915127af4ef7ac8f47f4d1d4bfe1ce16
JM De Ocampo <jmdoescode@gmail.com>
2019-11-20 09:57:35 -0800

    21.275 - Paging our Activity List

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -11,9 +14,23 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<ActivityDto>> { }
+        public class ActivitiesEnvelope
+        {
+            public List<ActivityDto> Activities { get; set; }
+            public int ActivityCount { get; set; }
+        }
+        public class Query : IRequest<ActivitiesEnvelope>
+        {
+            public Query(int? limit, int? offset)
+            {
+                Limit = limit;
+                Offset = offset;
+            }
+            public int? Limit { get; set; }
+            public int? Offset { get; set; }
+        }
 
-        public class Handler : IRequestHandler<Query, List<ActivityDto>>
+        public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -23,11 +40,20 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities.ToListAsync();
+                var queryable = _context.Activities.AsQueryable();
 
-                return _mapper.Map<List<Activity>, List<ActivityDto>>(activities);
+                var activities = await queryable
+                    .Skip(request.Offset ?? 0)
+                    .Take(request.Limit ?? 3)
+                    .ToListAsync();
+
+                return new ActivitiesEnvelope
+                {
+                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
+                    ActivityCount = queryable.Count()
+                };
             }
         }
     }
