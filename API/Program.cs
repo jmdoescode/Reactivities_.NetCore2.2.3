fd16c9565f961226869a7b674b923160126fa934
JM De Ocampo <jmdoescode@gmail.com>
2019-12-02 13:44:01 -0800

    FIX for error: HTTP Error 500.30 - ANCM In-Process Start Failure

--- API/Program.cs
+++ API/Program.cs
@@ -38,7 +38,7 @@ namespace API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(x => x.AddServerHeader = false)
+                //.UseKestrel(x => x.AddServerHeader = false)
                 .UseStartup<Startup>();
     }
 }
